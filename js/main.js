a85ab8dfddcf2a9633c8fda9079c5f309f55cebb
Joy Xie <a556987@gmail.com>
2021-01-05 17:29:21 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,56 +1,77 @@
 $(function(){
     let $takeshot_btn = $('#js-takeshot-btn');
-    let $colsed_btn = $('#js-colsed-btn');
-    let $output_warp = $('.output-wrap');
-    let $edit_btn_array = $('.edit-btn');
+    let $colsed_modal_btn = $('#js-colsed-btn');
+
     let $year_wrap = $('#js-year-wrap');
+    let $grid_wrap = $('#js-grid-wrap');
+    let $output_warp = $('.output-wrap');
 
     let today = new Date();
     let current_year = today.getFullYear();
 
-    
-    $edit_btn_array.each(function(index){
-        let $img_wrap = $(this).siblings('img');
+    let count = 4;
 
-        $(this).on('change', function(event){
-            /* refrence: http://jsfiddle.net/LvsYc/ */
-            if (this.files && this.files[0]) {
-                var reader = new FileReader();
-                
-                reader.onload = function(e) {
-                    $img_wrap.attr('src', e.target.result);
-                }
-                
-                reader.readAsDataURL(this.files[0]); // convert to base64 string
-            }
+    function createImagesWrap() {
+        for(let i=0 ; i < count*count ; i++) {
+            $grid_wrap.append(
+                `<div class="image-wrap">
+                    <input type="file" class="edit-btn">
+                    <img src="#" alt="your image" />
+                </div>`
+            );
+        }
+
+        $grid_wrap.css({
+            'grid-template-columns': `repeat(${count}, 1fr)`,
+            'grid-template-rows': `repeat(${count}, 1fr)`
         });
-    });
+    }
 
+    function bindImageEvent() {
+        $('.edit-btn').each(function(index){
+            let $img_wrap = $(this).siblings('img');
+
+            $(this).on('change', function(event){
+                /* refrence: http://jsfiddle.net/LvsYc/ */
+                if (this.files && this.files[0]) {
+                    var reader = new FileReader();
+                    
+                    reader.onload = function(e) {
+                        $img_wrap.attr('src', e.target.result);
+                    }
+                    
+                    reader.readAsDataURL(this.files[0]); // convert to base64 string
+                }
+            });
+        });
+    }
+
+    async function build() {
+        await createImagesWrap();
+        await bindImageEvent();
+    }
+
+    function getScreenshot() {
+        let $output = $('#output');
+        $output.empty();
+
+        html2canvas(document.querySelector("#capture")).then(canvas => {
+            $output.append(canvas);
+        });
+    }
+    
     $takeshot_btn.on('click', function(){
         getScreenshot();
         $output_warp.addClass('show');
     });
 
-    $colsed_btn.on('click', function(){
+    $colsed_modal_btn.on('click', function(){
         $output_warp.removeClass('show');
     });
 
-    function getScreenshot() {
-        // let $screenshot_wrap = $('#capture');
-        // $screenshot_wrap.css({
-        //     'height': 'auto'
-        // });
-
-        // 輸出容器
-        let $output = $('#output');
-        $output.empty();
-
-        html2canvas(document.querySelector("#capture")).then(canvas => {
-            // 輸出容器
-            $output.append(canvas);
-        });
-    }
+    
 
     /* init */
+    build();
     $year_wrap.text(current_year);
 });
